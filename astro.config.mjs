50ee6e4bf319ec517efa3adbc04109260b16775e
Ciaran Finn <ciaranfinn@mac.lan>
2025-07-05 15:09:33 -0400

    setup website basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ciaranfinn.dev",
+  base: "/",
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
+});
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const WEBSITE_TITLE = "Ciaran Finn";
+export const WEBSITE_DESCRIPTION = "My personal website";
